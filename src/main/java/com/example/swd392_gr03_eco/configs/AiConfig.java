d37ffde8dd92a79a8ccb8c1e76744ca9d42b725f
Long <longvtse184610@fpt.edu.vn>
2026-03-11 22:27:45 +0700

    change ai embedding

--- src/main/java/com/example/swd392_gr03_eco/configs/AiConfig.java
+++ src/main/java/com/example/swd392_gr03_eco/configs/AiConfig.java
@@ -1,15 +1,23 @@
 package com.example.swd392_gr03_eco.configs;
 
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+//import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiEmbeddingModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
 
+    @Value("${gemini.api-key}")
+    private String apiKey;
+
     @Bean
     EmbeddingModel embeddingModel() {
-        return new AllMiniLmL6V2EmbeddingModel();
+        return GoogleAiEmbeddingModel.builder()
+                .apiKey(apiKey)
+                .modelName("gemini-embedding-001")
+                .build();
     }
 }
