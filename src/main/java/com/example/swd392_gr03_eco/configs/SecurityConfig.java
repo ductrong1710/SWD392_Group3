e79cc7cf7a258dd2a84735e0f64a4958a2f172c0
Long <longvtse184610@fpt.edu.vn>
2026-03-12 17:14:55 +0700

    fix security

--- src/main/java/com/example/swd392_gr03_eco/configs/SecurityConfig.java
+++ src/main/java/com/example/swd392_gr03_eco/configs/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "http://127.0.0.1:3000",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "https://swd392-group3-fe.onrender.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
