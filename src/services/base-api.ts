2cb5bfb131787690bcb3351aa294a9c1a97e34a6
Long <longvtse184610@fpt.edu.vn>
2026-03-16 09:29:26 +0700

    lastest commit for deploy

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://swd392-group3-be.onrender.com/api";
 
 // ============================================
 // AUTH TOKEN MANAGEMENT
