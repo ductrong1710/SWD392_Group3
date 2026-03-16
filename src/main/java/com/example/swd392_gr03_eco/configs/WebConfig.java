2e6eb87fc44253446272abae9ec52ace4b4108c4
Long <longvtse184610@fpt.edu.vn>
2026-03-16 11:04:11 +0700

    fix cors render

---
+++ src/main/java/com/example/swd392_gr03_eco/configs/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.swd392_gr03_eco.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://swd392-group3-fe.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
